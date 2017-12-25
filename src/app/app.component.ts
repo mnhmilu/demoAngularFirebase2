5cc46020014d877915412d70fc799645ace4bd49
mnh_milu@yahoo.com <mnh_milu@yahoo.com>
2017-12-25 12:54:43 +0600

    crud firebase done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,149 +1,85 @@
 import { Component } from '@angular/core';
-import {AngularFireDatabase} from 'angularfire2/database';
+import { AngularFireDatabase } from 'angularfire2/database';
 import { Observable } from 'rxjs/Observable';
 import { OnDestroy } from '@angular/core/src/metadata/lifecycle_hooks';
 import { Subscription } from 'rxjs/Subscription';
-//import {  FirebaseListObservable } from "angularfire2/database";
-import {Teacher} from './teacher';
-
-
+import { Teacher } from './teacher';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
+export class AppComponent {
 
-  db:AngularFireDatabase;
- dd:Teacher;
-  /*
-  subscription: Subscription;
-  ngOnDestroy(): void {
-   this.subscription.unsubscribe();
-  } */
- 
-//  title = 'app';
+  db: AngularFireDatabase;
+  dd: Teacher;
 
-//teachers:any[];
+  teachers$: any;
+  teacher$;
+  constructor(db: AngularFireDatabase) {
+    this.db = db;
 
-teachers$:any;
-teacher$;
+    this.db.object('/Teachears/Teacher3').snapshotChanges().subscribe((snapshot) => {
+      console.log('KEY : ' + snapshot.key);
+    });
 
-//to prevent memory leak
+    db.database.ref('/Teachears/Teacher3').once('value').then(function (snapshot) {
+      var username = (snapshot.val() && snapshot.val().name) || 'Anonymous';
+      console.log("name is" + username);
+      // ...
+    });
 
 
-constructor(db:AngularFireDatabase) {
+    var updates = {};
 
-this.db=db;
+  }
 
-// reading multiple list
-//  this.db.list('/Teachears').subscribe(teachers=>{
-//   this.teachers$=teachers;
-//   });
 
-  // this.db.list('/Teachears').snapshotChanges().subscribe(snapshots=>{
+  list()
+  {
+    this.db.list('/Teachears').snapshotChanges().subscribe((snapshots) => {
+
+      this.teachers$ = snapshots;
+
+    });
+
+  }
+
+  add(teacher: HTMLInputElement, address: HTMLInputElement) {
+    this.db.list('/Teachears').push({ name: teacher.value, address: address.value });
+  }
+
+
+  update(teacher: any) {
+    console.log("Update operation is on going");
+    console.log(teacher.val());
+    console.log(teacher['key']);
+    console.log("debug 1" + teacher.val().name);
+    console.log("debug 2" + teacher.val().address);
+
+    this.db.list('Teachears').update(teacher['key'],{
+      name:teacher.val().name+' updated',
+      address:teacher.val().address
     
-  //   snapshots.forEach(snapshot => {
-  //     console.log('KEY : '+ snapshot.key);
-  //     console.log('VALUE : '+ snapshot.payload.val());
-      
-      
-  //   });
-  // });
+    });
+
+  }
+
+  delete(teacher: any) {
+
+    console.log("Delete operation is on going");
+    console.log(teacher['key']);
+    console.log("debug 1" + teacher.name);
+    console.log("debug 2" + teacher.$key);
+
+   //this.db.list
+
+    this.db.list('Teachears').remove( teacher['key']).then(x=>console.log("deleted"));
 
 
-  // this.db.list('/Teachears').valueChanges().subscribe((snapshots: Teacher[])=>{
-    
-  //   snapshots.forEach(snapshot => {
-  //     console.log(snapshot);
-  //     console.log('KEY : '+ snapshot.$key);
-  //     console.log('KEY : '+ snapshot.name);
-      
-      
-  //   });
-  // });
+  }
 
-// reading one object
-
-//var items =
-db.list('/Teachears').snapshotChanges().subscribe((snapshots)=>{
-    
-  this.teachers$=snapshots;
-   snapshots.forEach(snapshot => {
-       
-    console.log('KEY new : '+ snapshot.key);
-       
-       console.log("name "+snapshot.payload.val().name);
-       
-       console.log('address is : '+ snapshot.payload.val().address);
-       
-      
-      
-     });
-   });
-
- this.db.object('/Teachears/Teacher3').snapshotChanges().subscribe((snapshot)=>{
-
-  
-    console.log('KEY : '+ snapshot.key);
-    
-    
-
- });
-
-db.database.ref('/Teachears/Teacher3').once('value').then(function(snapshot) {
-  var username = (snapshot.val() && snapshot.val().name) || 'Anonymous';
-  console.log("name is"+username);
-  // ...
-});
-
-
- var updates = {};
- 
-
-
- //db.list('/Teachears').push({name:'ttt',address:'dddd'});
-
-
-  /* another way
-  
-this.subscription=db.list('/Teachears').valueChanges().subscribe(
-    teachers=> {
-      this.teachers=teachers;
-      console.log(this.teachers);
-  
-    }
-  );
-
-  */
-
-
-  
-
-
- 
-  /*
-  courses=>{
-    this.courses=courses;
-    console.log(this.courses);
-
-  })*/
-}
-
-add(teacher:HTMLInputElement,address:HTMLInputElement){
-  this.db.list('/Teachears').push({name:teacher.value,address:address.value});
-}
-
-
-update(teacher:any)
-{
- console.log(teacher['key']);
- console.log("debug 1"+ teacher.name);
- console.log("debug 2"+teacher.$key);
-
-  this.db.object('/Teachears/'+teacher.$key).set(teacher.$value +'Updated');
-}
 
 
 
